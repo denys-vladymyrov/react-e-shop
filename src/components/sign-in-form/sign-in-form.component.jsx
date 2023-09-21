c8fb5088205e47dad5a9ad63173569fa341393e1
Denys Vladymyrov <link_amd@yahoo.com>
2023-09-21 13:34:56 +0300

    added onAuthStateChange observer

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,9 +1,7 @@
-import { useState, useContext } from "react"
+import { useState } from "react"
 
 import FormInput from "../form-input/form-input.component"
 import Button from "../button/button.component"
-
-import { UserContext } from "../../context/user.context"
 
 import {
   createUserDocumentFromAuth,
@@ -22,11 +20,9 @@ const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields)
   const { email, password } = formFields
 
-  const { setCurrentUser } = useContext(UserContext)
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup()
-    await createUserDocumentFromAuth(user)
+    await signInWithGooglePopup()
   }
 
   const resetFormFields = () => {
@@ -38,8 +34,6 @@ const SignInForm = () => {
 
     try {
       const { user } = await signInAuthUserWithEmailAndPassword(email, password)
-
-      setCurrentUser(user)
       resetFormFields()
     }
     catch (error) {
