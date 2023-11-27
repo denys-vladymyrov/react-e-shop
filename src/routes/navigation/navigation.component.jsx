c858cf4444b64f209b9749d8202cdde5671f1208
Denys Vladymyrov <link_amd@yahoo.com>
2023-11-28 01:27:52 +0200

    added redux

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,20 +1,21 @@
 import { Fragment, useContext } from "react"
 import { Outlet } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 import { ReactComponent as Logo } from '../../assets/logo.svg'
 
 import CartIcon from "../../components/cart-icon/cart-icon.component"
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
 
-import { UserContext } from "../../context/user.context"
 import { CartContext } from "../../context/cart.context"
+import { selectCurrentUser } from "../../store/user/user.selector"
 
 import { signOutUser } from "../../utils/firebase/firebase.utils"
 
 import { NavigationContainer, LogoContainer, NavLinks, NavLink } from "./navigation.styles"
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext)
+  const currentUser = useSelector(selectCurrentUser)
   const { isCartOpen } = useContext(CartContext)
 
   const signOutHandler = async () => {
