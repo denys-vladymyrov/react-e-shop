b66772cf6bf7855e26daa5bb4387a2dd64332261
Denys Vladymyrov <link_amd@yahoo.com>
2023-12-10 01:39:04 +0200

    added redux thunk

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -2,18 +2,24 @@ import { Fragment } from "react"
 import { useSelector } from "react-redux"
 
 import CategoryPreview from "../../components/category-preview/caregory-preview.component"
-import { selectCategoriesMap } from "../../store/categories/category.selector"
+import Spinner from "../../components/spinner/spinner.component"
+
+import { selectCategoriesMap, selectCategoriesIsLoading } from "../../store/categories/category.selector"
 
 const CategoriesPreview = () => {
   const categoriesMap = useSelector(selectCategoriesMap)
+  const isLoading = useSelector(selectCategoriesIsLoading)
 
   return (
     <Fragment>
-      {
+      { isLoading ?
+        <Spinner />
+        :
         Object.keys(categoriesMap).map(( title ) => {
           const products = categoriesMap[title]
           return <CategoryPreview key={title} title={title} products={products} />
-        })}
+        })
+      }
     </Fragment>
   )
 }
