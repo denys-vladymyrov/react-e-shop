397f86b958a0b45a06bdbe27aa3e296b454de826
Denys Vladymyrov <link_amd@yahoo.com>
2023-12-28 21:31:52 +0200

    added stripe

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
-import reportWebVitals from './reportWebVitals'
 import { Provider } from "react-redux"
-// import { PersistGate } from "redux-persist/integration/react"
+import { Elements } from "@stripe/react-stripe-js"
 
 import App from './App'
-// import { UserProvider } from "./context/user.context"
-// import { CategoriesProvider } from "./context/categories.context"
-// import { CartProvider } from "./context/cart.context"
 import { store } from './store/store'
+import { stripePromise } from './utils/stripe/stripe.utils'
 
 import './index.scss'
 
@@ -17,16 +14,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/*<PersistGate persistor={persisstor}>*/}
+      <Elements stripe={stripePromise}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      {/*</PersistGate>*/}
+      </Elements>
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
